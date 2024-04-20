37f911739ecd924f00f37ee19264c9284aad93bb
Mirian Ocampo <ocampomirian74@gmail.com>
2024-04-19 13:24:24 -1200

    modificaciones en settings.py

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'python_heroku_task'
+    'python_heroku_task',
 ]
 
 MIDDLEWARE = [
